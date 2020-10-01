df76cd3349b81a86b0678f66a4851676c63bef03
Sourabh Kshirsagar <kshirsagarsousu18it@student.mes.ac.in>
2020-10-01 17:29:12 +0530

    second commit

--- Day2/6.1.php
+++ Day2/6.1.php
@@ -21,10 +21,10 @@
     $s2 = $_GET['s2'];
     $s3 = $_GET['s3'];
   
-   if ($s1==$s2 || $s2== $s3 || $s1== $s3){
-       echo "<b>Isosceles triangles</b>";
-    } elseif ($s1==$s2 && $s1==$s3 && $s2==$s3){
-        echo "<b>Equilateral triangles</b>";
+   if ($s1==$s2 && $s2== $s3 ){
+    echo "<b>Equilateral triangles</b>";
+    } elseif ($s1==$s2 || $s1==$s3 || $s2==$s3){
+        echo "<b>Isosceles triangles</b>";
    } else {
         echo "<b>Scalene triangle</b>";
     }
